b0e4f0537b35eb68131165d9ad0e8ed50a319673
dhawking <gaeta.d@gmail.com>
2015-07-26 00:54:40 -0700

    pushing auth status to navbar

--- app/app.js
+++ app/app.js
@@ -5,8 +5,24 @@ var so = angular.module('so', [
     'ui.bootstrap',
     'so.home',
     'so.auth',
+    'AuthService'
 ]);
 
 so.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/');
+
 });
+
+so.controller("soCtrl", ["$scope", "AuthService",
+	function($scope, AuthService) {
+		$scope.authData = AuthService.$getAuth();
+
+
+		if ($scope.authData) {
+		  console.log("Logged in as:", $scope.authData.uid);
+		} else {
+		  console.log("Logged out");
+		}
+
+	}
+])
